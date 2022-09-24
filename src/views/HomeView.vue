d4ae2d9410b78653ce10ed6cc6ab67cd405c99a9
pablexakd2022 <pabloalexandermujicalaime@gmail.com>
2022-09-24 17:15:52 -0400

    mi primer commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>This is an HOME page</h1>
+    <button class="btn btn-success"> click me</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+   
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,75 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="about">
+    <h1>Login</h1>
+    <div class="col-6 mx-auto">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" v-model="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" v-model="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" @click.prevent="submit()" class="btn btn-primary">Submit</button>
+</form>
+  </div>
+</div>
+</template>
+
+
+
+<script>
+
+export default {
+  data(){
+    return{
+      email:'',
+      password:''
+    }
+  },
+  methods:{
+    async submit(){
+      console.log(this.email);
+      console.log(this.password);
+
+      const requestOptions = {
+        method : "POST",
+        headers : {
+           "Content-type": "application/json",
+           "Access-Control-Allow-Origin" : "*"
+        },
+        body : JSON.stringify({email: "elon@gmail.com", password: "12345678"})
+      }
+      
+
+      fetch("http://localhost:3000/auth/login/", requestOptions).then(async response => {
+      try {
+       const data = await response.json()
+       console.log('response data?', data)
+      } catch(error) {
+        console.log('Error happened here!')
+        console.error(error)
+      }
+      })
+      
+      // fetch('http://localhost:3000/auth/login/', requestOptions)
+      // .then(response => response.json())
+
+
+      
+      // .then(data => {
+      //   console.log(data)
+        
+      // })
+
+    }
+  }
+}
+</script>
